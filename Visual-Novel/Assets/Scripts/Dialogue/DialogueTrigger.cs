907291e8750d514335f4fef070028d2705b68144
BlackAm <41035708+BlackAm@users.noreply.github.com>
2020-10-13 17:04:33 +0900

    Dialogue Changed To XML

--- Visual-Novel/Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Visual-Novel/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Xml;
 
 public class DialogueTrigger : MonoBehaviour
 {
-
     public Dialogue dialogue;
+
+    public string xmlFileName;
+
+    private int dialogueCount = 0;
 
     public void TriggerDialogue()
     {
@@ -19,8 +23,32 @@ public class DialogueTrigger : MonoBehaviour
 
     private void Awake()
     {
-        dialogue.sentences[0] = "마이크 체크 원투";
-        dialogue.sentences[1] = "잘 나오나 테스트중";
-        dialogue.sentences[2] = GameManager.instance.HeroName + " 이름까지 잘 나오는 중";
+        LoadXML(xmlFileName);
+    }
+
+    private void LoadXML(string _fileName)
+    {
+        TextAsset txtAsset = (TextAsset)Resources.Load("XML/" + _fileName);
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(txtAsset.text);
+
+        // 하나씩 가져오기 테스트 예제.
+        /* XmlNodeList cost_Table = xmlDoc.GetElementsByTagName("dialogue");
+
+        foreach (XmlNode cost in cost_Table)
+        {
+            dialogue.sentences[dialogueCount] = cost.InnerText;
+            dialogueCount++;
+        } */
+
+
+        // 전체 아이템 가져오기 예제.
+        XmlNodeList all_nodes = xmlDoc.SelectNodes("dataroot/TestName");
+        foreach (XmlNode node in all_nodes)
+        {
+            dialogue.names[dialogueCount] = node.SelectSingleNode("name").InnerText;
+            dialogue.sentences[dialogueCount] = node.SelectSingleNode("dialogue").InnerText;
+            dialogueCount++;
+        }
     }
 }
---
+++ Visual-Novel/Assets/Scripts/Prologue/PrologueTrigger.cs
@@ -0,0 +1,55 @@
+﻿using System.Xml;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrologueTrigger : MonoBehaviour
+{
+    public Prologue prologue;
+
+    public string xmlFileName;
+
+    private int dialogueCount = 0;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<PrologueManager>().StartDialogue(prologue);
+    }
+
+    private void Start()
+    {
+        TriggerDialogue();
+    }
+
+    private void Awake()
+    {
+        LoadXML(xmlFileName);
+    }
+
+    private void LoadXML(string _fileName)
+    {
+        TextAsset txtAsset = (TextAsset)Resources.Load("XML/" + _fileName);
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(txtAsset.text);
+
+        // 하나씩 가져오기 테스트 예제.
+        XmlNodeList cost_Table = xmlDoc.GetElementsByTagName("dialogue");
+
+        foreach (XmlNode cost in cost_Table)
+        {
+            Debug.Log(cost.InnerText);
+            prologue.sentences[dialogueCount] = cost.InnerText;
+            dialogueCount++;
+        }
+
+
+        // 전체 아이템 가져오기 예제.
+        /*XmlNodeList all_nodes = xmlDoc.SelectNodes("dataroot/TestDialogue");
+        foreach (XmlNode node in all_nodes)
+        {
+            // 수량이 많으면 반복문 사용.
+            Debug.Log("[at once] id :" + node.SelectSingleNode("id").InnerText);
+            Debug.Log("[at once] dialogue : " + node.SelectSingleNode("dialogue").InnerText);
+        }*/
+    }
+}
